742087798f1dc790c41736db3deaf9c8cf15f3cf
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2024-06-02 16:12:30 +0200

    Calculate method started

--- Calculator.cs
+++ Calculator.cs
@@ -1,19 +1,34 @@
 namespace RpnCalculator;
 
-public class Calculator(List<string> acceptedOperators) {
-     private List<string> AcceptedOperators { get; } = acceptedOperators;
+public class Calculator() {
+     private List<string> AcceptedOperators { get; } = ["+", "-", "*", "/"];
 
      private Stack<Token> stack = new();
 
      /// Receives tokens in order (e.g. 4 6 + 3 *)
-     /// Pops them onto the stack starting from front (e.g. * 3 + 6 4)
-     private void FillStack(List<Token> tokens) {
-          tokens.ForEach(token => stack.Push(token));
-     }
+     /// Pushes them onto the stack starting from front (e.g. * 3 + 6 4)
+     // private void FillStack(List<Token> tokens) {
+     //      if(tokens.Count == 0) 
+     //           throw new ArgumentException("No tokens provided", tokens.ToString());
+     //      tokens.Reverse(); // Mutates list
+     //      tokens.ForEach(token => stack.Push(token));
+     // }
 
      public double Calculate(List<Token> tokens) {
           // put tokens into stack and calculate
-          while(stack.Count > 1)
-          return new double();
+          tokens.ForEach(token => {
+               if(token.IsOperand()) 
+                    stack.Push(token);
+               else {
+                    // Pop two operands from stack
+                    Token[] arr = [stack.Pop(), stack.Pop()];
+                    // Perform operation
+                         //  [0] is right operand,
+                         //  [1] is left operand,
+                         //  operator is this token 
+                    // token.GetValue()
+               }
+
+          });
      }
 }
