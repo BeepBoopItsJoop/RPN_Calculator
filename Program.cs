a5ef84044247b46fe9bcbd3f6a5cdafa8a8b0312
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2024-06-06 17:19:37 +0200

    Tested Calculate Method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using RpnCalculator;
+
+Calculator calculator = new();
+var ExampleTokens = new List<Token> {
+     new Token(TokenType.Operand, "4"),
+     new Token(TokenType.Operand, "2"),
+     new Token(TokenType.Operand, "3"),
+     new Token(TokenType.Operand, "5"),
+     new Token(TokenType.Operand, "1"),
+     new Token(TokenType.Operator, "-"),
+     new Token(TokenType.Operator, "+"),
+     new Token(TokenType.Operator, "*"),
+     new Token(TokenType.Operator, "+"),
+};
+var bleep = calculator.Calculate(ExampleTokens);
+;
